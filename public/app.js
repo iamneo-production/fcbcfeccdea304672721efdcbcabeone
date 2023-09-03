31aef124fa484ad65de94ac9d188ec8db65cc049
Iamneo <techadmin@iamneo.ai>
2023-09-03 09:37:03 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -67,5 +67,10 @@
 let currentPlayer = 'X';
 let gameOver = false;
 function makeMove(cell){
-    if(cell.innerHTML == "&&"!)
+    if(cell.innerHTML == '' && !gameOver){
+        cell.innerHTML = currentPlayer;
+        checkWinner();
+        currentPlayer = currentPlayer == 'X' ?'O':'X';\
+        document.querySelector
+    }
 }
